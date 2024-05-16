72e05f02be3750dbfa77be042341b42e23cd7570
vantukk2222 <ntu96109@gmail.com>
2024-05-16 12:31:15 +0700

    chore: Refactor CategoryProvider component in categoryContext.jsx

--- sourceFE/src/context/categoryContext.jsx
+++ sourceFE/src/context/categoryContext.jsx
@@ -1,36 +1,38 @@
-import {  createContext, useState } from "react";
-import PropTypes from 'prop-types'
+import { createContext, useState } from "react";
+import PropTypes from "prop-types";
 
 const CategoryContext = createContext({});
-export const CategoryProvider = ({children}) => {
-    const [categoryData, setCategoryData] = useState([]);
-    const [listCategory, setListCategory] = useState([]);
-    const [filter, setFilter] = useState({
-        page : 1 , 
-        pageSize : 4, 
-        sortField : 'name',
-        sortOrder:'desc'
-    })
-    const [page, setPage] = useState({
-        totalPages :1,
-        currentPage :1
+export const CategoryProvider = ({ children }) => {
+  const [categoryData, setCategoryData] = useState([]);
+  const [listCategory, setListCategory] = useState([]);
+  const [filter, setFilter] = useState({
+    page: 1,
+    pageSize: 4,
+    sortField: "name",
+    sortOrder: "desc",
+  });
+  const [page, setPage] = useState({
+    totalPages: 1,
+    currentPage: 1,
+  });
 
-    });
-
-    const contextCategoryData = {
-       categoryData,
-       setCategoryData,
-       listCategory,
-       setCategoryData,
-       filter,
-       setFilter,
-       page,
-       setPage
-    };
-    return <CategoryContext.Provider value={contextCategoryData}>{children}</CategoryContext.Provider>
+  const contextCategoryData = {
+    categoryData,
+    setCategoryData,
+    listCategory,
+    filter,
+    setFilter,
+    page,
+    setPage,
+  };
+  return (
+    <CategoryContext.Provider value={contextCategoryData}>
+      {children}
+    </CategoryContext.Provider>
+  );
 };
 
 CategoryProvider.propTypes = {
-    children : PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired,
 };
 export default CategoryContext;
